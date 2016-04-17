3cecfce0e12d1918a0829052dfda579af1cbe588
clitetailor <ducnhatphung@gmail.com>
2016-04-17 10:42:56 +0700

    Day 2

--- arp.py
+++ arp.py
@@ -4,39 +4,50 @@ class Type:
 	Arp = bytes.fromhex("0806")
 
 class HardwareType:
-	Ethernet = bytes.fromhex("0001")
+    Ethernet = bytes.fromhex("0001")
 
 class ProtocolType:
-	IPv4 = bytes.fromhex("0800")
+    IPv4 = bytes.fromhex("0800")
 
 class HardwareAddressLength:
-	MAC = bytes.fromhex("06")
+    MAC = bytes.fromhex("06")
 
 class ProtocolAddressLength:
-	IPv4 = bytes.fromhex("04")
-	IPv6 = bytes.fromhex("06")
+    IPv4 = bytes.fromhex("04")
+    IPv6 = bytes.fromhex("06")
 
 class OperationCode:
-	Request = bytes.fromhex("0001")
-	Reply = bytes.fromhex("0002")
+    Request = bytes.fromhex("0001")
+    Reply = bytes.fromhex("0002")
 
 
-def SendArp(host_ip, host_mac_address, network_interface, gateway_ip):
-	
-	rawsocket = socket.socket(socket.AF_INET, socket.SOCK_RAW)
-	rawsocket = socket.connect((network_interface, 0))
-	
-	packet = CreateArpPacket(host_ip, host_mac_address, network_interface, gateway_ip)
-	
-	rawsocket.send(packet)
-	return
+def SendArp(network_interface, packet):
+    
+    rawsocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+    rawsocket.bind((network_interface, 0))
+    
+    rawsocket.send(packet)
+    return
 
 def CreateArpPacket(host_ip, host_mac_address, network_interface, gateway_ip):
-        packet = bytes(map(int, gateway_ip.split('.') )) + bytes(map(int, host_ip.split('.') )) + Type.Arp + HardwareType.Ethernet + ProtocolType.IPv4 + HardwareAddressLength.MAC + ProtocolAddressLength.IPv4 + OperationCode.Request + bytes.fromhex(host_mac_address.replace(":", "")) + bytes.fromhex(host_ip) + bytes.fromhex("ffffffffffff") + bytes.fromhex(map(int, gateway_ip.split('.') ))
-
-        
-        return packet
+    #Ethernet Layer
+    packet = bytes.fromhex("ff ff ff ff ff ff")
+    packet = packet + bytes.fromhex( host_mac_address.replace(":", "") )
+    packet = packet + Type.Arp
+    #Arp Layer
+    packet = packet + HardwareType.Ethernet
+    packet = packet + ProtocolType.IPv4
+    packet = packet + HardwareAddressLength.MAC
+    packet = packet + ProtocolAddressLength.IPv4
+    packet = packet + OperationCode.Request
+    packet = packet + bytes.fromhex(host_mac_address.replace(":", ""))
+    packet = packet + socket.inet_aton(host_ip)
+    packet = packet + bytes.fromhex("ffffffffffff")
+    packet = packet + socket.inet_aton(gateway_ip)
+    
+    
+    return packet
 
 if __name__ == "__main__":
-        print(CreateArpPacket("192.168.1.104", "28:c2:dd:5d:eb:3d", "wlp3s0", "192.168.1.1"))
+    print(CreateArpPacket("192.168.1.101", "28:c2:dd:5d:eb:3d", "wlp3s0", "192.168.1.1"))
 

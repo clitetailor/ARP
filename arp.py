5054f0ae0a9c505d27e92074c61b94df186625e6
clitetailor <ducnhatphung@gmail.com>
2016-04-17 16:07:25 +0700

    Day 2

--- arp.py
+++ arp.py
@@ -1,5 +1,10 @@
 import socket
+import netifaces as ni
 
+# Enumerate
+#
+#
+#
 class Type:
 	Arp = bytes.fromhex("0806")
 
@@ -7,47 +12,77 @@ class HardwareType:
     Ethernet = bytes.fromhex("0001")
 
 class ProtocolType:
-    IPv4 = bytes.fromhex("0800")
+        IPv4 = bytes.fromhex("0800")
 
 class HardwareAddressLength:
-    MAC = bytes.fromhex("06")
+        MAC = bytes.fromhex("06")
 
 class ProtocolAddressLength:
-    IPv4 = bytes.fromhex("04")
-    IPv6 = bytes.fromhex("06")
+        IPv4 = bytes.fromhex("04")
+        IPv6 = bytes.fromhex("06")
 
 class OperationCode:
-    Request = bytes.fromhex("0001")
-    Reply = bytes.fromhex("0002")
+        Request = bytes.fromhex("0001")
+        Reply = bytes.fromhex("0002")
 
 
+# Methods
+#
+#
+#
 def SendArp(network_interface, packet):
-    
-    rawsocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
-    rawsocket.bind((network_interface, 0))
-    
-    rawsocket.send(packet)
-    return
+        rawsocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+        rawsocket.bind((network_interface, 0))
+        
+        rawsocket.send(packet)
+        return
 
-def CreateArpPacket(host_ip, host_mac_address, network_interface, gateway_ip):
-    #Ethernet Layer
-    packet = bytes.fromhex("ff ff ff ff ff ff")
-    packet = packet + bytes.fromhex( host_mac_address.replace(":", "") )
-    packet = packet + Type.Arp
-    #Arp Layer
-    packet = packet + HardwareType.Ethernet
-    packet = packet + ProtocolType.IPv4
-    packet = packet + HardwareAddressLength.MAC
-    packet = packet + ProtocolAddressLength.IPv4
-    packet = packet + OperationCode.Request
-    packet = packet + bytes.fromhex(host_mac_address.replace(":", ""))
-    packet = packet + socket.inet_aton(host_ip)
-    packet = packet + bytes.fromhex("ffffffffffff")
-    packet = packet + socket.inet_aton(gateway_ip)
-    
-    
-    return packet
+def NetworkInterfaces():
+        return ni.interfaces()
 
+def InterfaceMacAddresses(network_interface):
+        mac_addresses = []
+        for mac_info in ni.ifaddresses(network_interface)[ni.AF_PACKET]:
+                mac_addresses.append(mac_info['addr'])
+        return mac_addresses
+
+def InterfaceIpAddresses(network_interface):
+        ip_addresses = []
+        for ip_info in ni.ifaddresses(network_interface)[ni.AF_INET]:
+                ip_addresses.append(ip_info['addr'])
+        return ip_addresses
+
+def CreateArpPacket(sender_mac_address, sender_ip_address, target_ip_address):
+        
+        #Ethernet Layer
+        packet = bytes.fromhex("ff ff ff ff ff ff")
+        packet = packet + bytes.fromhex( sender_mac_address.replace(":", "") )
+        packet = packet + Type.Arp
+
+        #Arp Layer
+        packet = packet + HardwareType.Ethernet
+        packet = packet + ProtocolType.IPv4
+        packet = packet + HardwareAddressLength.MAC
+        packet = packet + ProtocolAddressLength.IPv4
+        packet = packet + OperationCode.Request
+
+        #Data
+        packet = packet + bytes.fromhex(sender_mac_address.replace(":", ""))
+        packet = packet + socket.inet_aton(sender_ip_address)
+        packet = packet + bytes.fromhex("ffffffffffff")
+        packet = packet + socket.inet_aton(target_ip_address)
+        
+        return packet
+
+# Main Program
+#
 if __name__ == "__main__":
-    print(CreateArpPacket("192.168.1.101", "28:c2:dd:5d:eb:3d", "wlp3s0", "192.168.1.1"))
+        
+	interface = NetworkInterfaces()[2]
+	mac = InterfaceMacAddresses(interface)[0]
+	ip = InterfaceIpAddresses(interface)[0]
+	
+	gateway = "192.168.1.1"
 
+	packet = CreateArpPacket(mac, ip, gateway)
+	SendArp(interface, packet)
